640b5a06e2c8fbbd1e84153dd0c217525c2c7937
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2026-02-26 21:27:14 +0100

    added login logout pages via crispy forms, bootstrap4 and some tinketing with navbar and index page

--- movie_review_app/views.py
+++ movie_review_app/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+from movie_review_app.models import Movie, Review
+
+
 def index(request):
-    return render(request, "movie_review/index.html")
+    """
+    Main Page view function
+    """
+    num_movies = Movie.objects.count()
+    num_reviews = Review.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_movies": num_movies,
+        "num_reviews": num_reviews,
+        "num_visits": num_visits
+    }
+
+    return render(request, "movie_review/index.html", context)
