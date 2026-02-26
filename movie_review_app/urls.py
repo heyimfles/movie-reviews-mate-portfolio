98993a35a9cc7cc8a73b002ed44f63d80f8cbc33
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2026-02-26 22:43:20 +0100

    added create forms for review and movie with all fields and added those to list pages

--- movie_review_app/urls.py
+++ movie_review_app/urls.py
@@ -2,11 +2,19 @@ from django.urls import path
 
 from .views import (
     index,
+    MovieListView,
+    ReviewListView,
+    MovieCreateView,
+    ReviewCreateView,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("movies/", MovieListView.as_view(), name="movie_list"),
+    path("reviews/", ReviewListView.as_view(), name="review_list"),
+    path("movies/create/", MovieCreateView.as_view(), name="movie_create"),
+    path("reviews/create/", ReviewCreateView.as_view(), name="review_create"),
 ]
 
 app_name = "movie_review"
--- movie_review_app/views.py
+++ movie_review_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-from movie_review_app.models import Movie, Review
+from movie_review_app.models import (
+    Movie,
+    Review,
+)
 
 
 def index(request):
@@ -20,3 +25,31 @@ def index(request):
     }
 
     return render(request, "movie_review/index.html", context)
+
+
+class MovieListView(generic.ListView):
+    model = Movie
+    context_object_name = "movie_list"
+    template_name = "movie_review/movie_list.html"
+    paginate_by = 10
+
+
+class MovieCreateView(generic.CreateView):
+    model = Movie
+    fields = "__all__"
+    success_url = reverse_lazy("movie_review:movie_list")
+    template_name = "movie_review/movie_form.html"
+
+
+class ReviewListView(generic.ListView):
+    model = Review
+    context_object_name = "review_list"
+    template_name = "movie_review/review_list.html"
+    paginate_by = 5
+
+
+class ReviewCreateView(generic.CreateView):
+    model = Review
+    fields = "__all__"
+    success_url = reverse_lazy("movie_review:review_list")
+    template_name = "movie_review/review_form.html"
